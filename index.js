200ff0674042184da8c9a1a17aab4604715c06f1
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-06-24 20:42:49 +0530

    updated the file time request scehma

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const MentorRoutes = require("./routes/mentor");
-const BookmarkRoutes = require("./routes/bookmark");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('DB Connected!')).catch((err) => console.log(err))
@@ -13,5 +12,4 @@ app.use(express.json());
 app.use("/api/", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/mentor", MentorRoutes);
-app.use("/api/bookmark", BookmarkRoutes);
 app.listen(process.env.PORT || 5002, () => console.log(`app listening on port ${process.env.PORT}!`))
