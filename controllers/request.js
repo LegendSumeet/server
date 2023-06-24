a48c650ddbe99343205806b3a922112a6a7a4dfc
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-06-24 21:13:11 +0530

    fix normal bugs

--- controllers/request.js
+++ controllers/request.js
@@ -102,11 +102,6 @@ const acceptTimeRequest = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-
-
-
-
-
 module.exports = {
     createRequest,
     getTimeRequestsByMentor,
