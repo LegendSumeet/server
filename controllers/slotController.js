d672ce6296471ad0cff76c82bc400524b7fe4fef
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-07-18 19:28:43 +0530

    g

--- controllers/slotController.js
+++ controllers/slotController.js
@@ -45,6 +45,11 @@ const getSlotsByUserAndMentorId = async (req, res) => {
     try {
         const { userID, mentorID , requestID } = req.params;
         const slots = await Slot.find({ mentorID: mentorID, userID: userID ,requestID:requestID });
+
+        if (slots.length === 0) {
+            return res.status(404).json({ error: 'No slots found for the given parameters' });
+        }
+        
         res.status(200).json(slots);
     } catch (error) {
         res.status(500).json({ error: error.message });
