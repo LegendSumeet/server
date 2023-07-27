acb7a86c3341ae5292f99b7117afe0af7634511c
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-07-27 11:59:07 +0530

    ee

--- controllers/chatcontroller.js
+++ controllers/chatcontroller.js
@@ -1,17 +1,28 @@
-const  Chat  = require("../models/chat");
+const Chat = require("../models/chat");
 const User = require("../models/User");
 
 module.exports = {
-
-  createChat : async (req, res) => {
+  createChat: async (req, res) => {
     try {
-      const { chatName, isGroupChat, user , mentor} = req.body;
-      const chat = new Chat({ chatName, isGroupChat, user , mentor });
+      const { chatName, isGroupChat, user, mentor } = req.body;
+      const chat = new Chat({ chatName, isGroupChat, user, mentor });
       await chat.save();
-      res.status(201).json({ message: 'Chat created successfully', chat });
+      res.status(201).json({ chat });
     } catch (error) {
       res.status(500).json({ error: 'An error occurred while creating the chat' });
     }
   },
 
-}
+  getChat: async (req, res) => {
+    try {
+      const chatId = req.params.id; 
+      const chat = await Chat.findById(chatId);
+      if (!chat) {
+        return res.status(404).json({ message: 'Chat not found' });
+      }
+      res.status(200).json({ chat });
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the chat' });
+    }
+  }
+};
