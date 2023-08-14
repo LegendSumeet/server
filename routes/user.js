e1cb98121ada3a4892d62d0ef0e0ae4a880a2153
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2023-08-14 23:05:46 +0530

    aa

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,24 @@
 const router = require("express").Router();
 const { Router } = require("express");
-const { createRating,getMentorReviews , getAllReviews  } = require('../controllers/reviewcontroller');
+const { createRating, getMentorReviews, getAllReviews } = require('../controllers/reviewcontroller');
 const userController = require("../controllers/userController");
-const { verifyTokenAndAuthorization ,verifyTokenAndAdmin, verifyToken} = require("../middleware/verifyToken");
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin, verifyToken } = require("../middleware/verifyToken");
 const { getTimeRequestsByUser } = require("../controllers/requestController");
 const slotscontroller = require("../controllers/slotController")
 
 //auth
 router.put("/", verifyToken, userController.updateUser);
 router.delete("/", verifyToken, userController.deleteUser);
-router.get("/:id",userController.getUser);
+router.get("/:id", userController.getUser);
 //time request
-router.get('/:seekerId/time-requests',getTimeRequestsByUser );
+router.get('/:seekerId/time-requests', getTimeRequestsByUser);
 
 //check
-router.get('/check/:userId',userController.MentorOrUser);
-router.get('/cnflsots/:requestID',slotscontroller.confirmedSlottoUser)
+router.get('/check/:userId', userController.MentorOrUser);
+router.get('/cnflsots/:requestID', slotscontroller.confirmedSlottoUser)
 
 
 
 
 
 module.exports = router;
- 
